bed8c5bc61c7686d105c3fc0f8eb0a730245961e
Reasky <94378921+ReaskyDev@users.noreply.github.com>
2024-07-17 20:48:58 +0300

    upd

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb+srv://nikitamail3358:I3bLUJokcDYWv7qB@test.hbyimwu.mongodb.net/?retryWrites=true&w=majority&appName=test&tls=true&tlsAllowInvalidCertificates=true",
-    exchangeRate: "your-exchangeratesapi-key",
+    mongoURI: "mongodb+srv://nikitamail3358:<password>@test.hbyimwu.mongodb.net/?retryWrites=true&w=majority&appName=test&tls=true&tlsAllowInvalidCertificates=true",
+    exchangeRate: "token",
 
     mongoDB: "test"
 };
